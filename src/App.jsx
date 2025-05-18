42725ef42325970d7002241faed432fbcc16dfae
Rasel-Islam007 <raselvhai132@gmail.com>
2025-05-18 23:17:54 +0600

    Payment Issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Cards from "./Cards";
 const App = () => {
   return (
     <div className=" min-h-screen bg-black m-auto">
-<div className="text-2xl text-red-700">Due to payment issues, this website is currently blocked by<div/>
+<div className="text-2xl text-red-700">Due to payment issues, this website is currently blocked by</div>
   <div className="text-4xl">Saleh Adib Hasnat</div>
     </div>
   );
