c109316d86f2bcaeda062162c255970d30af3f1f
Rasel-Islam007 <raselvhai132@gmail.com>
2025-05-18 23:29:25 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import Cards from "./Cards";
 
 const App = () => {
   return (
-    <div className=" min-h-screen bg-black m-auto">
-<div className="text-2xl text-red-700">Due to payment issues, this website is currently blocked by</div>
-  <div className="text-4xl">Saleh Adib Hasnat</div>
+    <div className=" min-h-screen bg-black">
+      <Navbar />
+
+      <Cards />
     </div>
   );
 };
