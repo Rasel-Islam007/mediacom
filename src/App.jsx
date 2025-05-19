5be3295dae40c0dc7dca84823025b4796f48c30e
Rasel-Islam007 <raselvhai132@gmail.com>
2025-05-19 23:50:12 +0600

    Payment Issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Cards from "./Cards";
 
 const App = () => {
   return (
-    <div className=" min-h-screen bg-black w-full m-auto">
-      <div className="text-center">
+    <div className=" min-h-screen bg-black w-full relative">
+      <div className="text-center absolute top-0 bottom-0 left-0 right-0 m-auto">
       <div className=" text-2xl text-red-700">This website has been shut down for payment issues by</div>
         <div className="text-4xl">Saleh Adib Hasnat</div>
       </div>
